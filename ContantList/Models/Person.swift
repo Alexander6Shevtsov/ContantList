95206c3d338eec55baaf1bca18584a3bb3f03ba5
Alexander6Shevtsov <axrsvr@gmail.com>
2025-04-16 18:35:19 +0300

    Add Model

---
+++ ContantList/Models/Person.swift
@@ -0,0 +1,43 @@
+//
+//  Person.swift
+//  ContantList
+//
+//  Created by Alexander Shevtsov on 16.04.2025.
+//
+
+struct Person: Identifiable {
+    
+    let id: Int
+    let name: String
+    let surname: String
+    let email: String
+    let phoneNumber: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+    
+    static func getContactList() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = DataStore.shared.names.shuffled()
+        let surnames = DataStore.shared.surnames.shuffled()
+        let emails = DataStore.shared.emails.shuffled()
+        let phones = DataStore.shared.phones.shuffled()
+        
+        for index in 0..<names.count {
+            let person = Person(
+                id: index + 1,
+                name: names[index],
+                surname: surnames[index],
+                email: emails[index],
+                phoneNumber: phones[index]
+            )
+            
+            persons.append(person)
+        }
+        
+        return persons
+    }
+}
---
+++ ContantList/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ContantList
+//
+//  Created by Alexander Shevtsov on 16.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+  //  private let contacts = 
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
